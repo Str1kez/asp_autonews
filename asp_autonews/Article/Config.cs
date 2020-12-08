eafdd19966b337a7ca2357443fef0d14f74ba726
Str1kez <vazinov@yandex.ru>
2020-12-08 23:31:10 +0300

    Link with DB

---
+++ asp_autonews/Article/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_autonews.Article
+{
+    public class Config
+    {  
+        public static string ConnectionString { get; set; }
+        public static string SiteName { get; set; }
+        public static string SiteInfo { get; set; }
+        public static string SiteEmail { get; set; }
+    }
+}
--- asp_autonews/Startup.cs
+++ asp_autonews/Startup.cs
@@ -1,4 +1,4 @@
- using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,7 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
- using Microsoft.AspNetCore.Mvc;
+using asp_autonews.Article;
+using Microsoft.AspNetCore.Mvc;
 
  namespace asp_autonews
 {
@@ -24,6 +25,7 @@ using System.Threading.Tasks;
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.Bind("Project", new Config());
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
