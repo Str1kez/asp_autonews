8fd1c15a67e2c4813732a03b9c7bfd6fec78ae15
Str1kez <vazinov@yandex.ru>
2020-12-20 04:09:42 +0300

    creation and modification of information

--- asp_autonews/Areas/Admin/Controllers/ArticlesController.cs
+++ asp_autonews/Areas/Admin/Controllers/ArticlesController.cs
@@ -1,13 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.IO;
+using asp_autonews.Domain;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace asp_autonews.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ArticlesController : Controller
     {
-        // GET
-        public IActionResult Index()
+        private readonly Manager dataManager;
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public ArticlesController(Manager dataManager, IWebHostEnvironment hostEnvironment)
         {
-            return View();
+            this.dataManager = dataManager;
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Edit(Guid id)
+        {   
+            // Если айди дефолтный = новый -> создаем статью, иначе берем её из БД
+            var entity = id == default ? new Domain.Entities.Article() : dataManager.Articles.GetArticleById(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        // IFormFile Для подгрузки картинок
+        public  IActionResult Edit (Domain.Entities.Article model, IFormFile img)
+        {
+            if (ModelState.IsValid)
+            {
+                if (img != null)
+                {
+                    model.Image = img.FileName;
+                    using (var s = new FileStream(Path.Combine(hostEnvironment.WebRootPath, "img/", img.FileName),
+                        FileMode.Create) )
+                    {
+                        img.CopyTo(s);
+                    }
+                }
+                // Сохраняем и переходим на главную страницу в админке
+                dataManager.Articles.SaveArticle(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController)
+                    .Replace("Controller", ""));
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Guid id)
+        {
+            dataManager.Articles.DeleteArticle(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController)
+                .Replace("Controller", ""));
         }
     }
 }
---
+++ asp_autonews/Areas/Admin/Controllers/InfoFieldsController.cs
@@ -0,0 +1,38 @@
+﻿using asp_autonews.Domain;
+using asp_autonews.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_autonews.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class InfoFieldsController : Controller
+    {
+        private readonly Manager dataManager;
+
+        public InfoFieldsController(Manager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Edit(string key)
+        {
+            // ищем в БД это текстовое поле и возвращаем в представление
+            var entity = dataManager.InfoFields.GetInfoFieldByKey(key);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(InfoField model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Если модель верная, то сохраняем в БД и перенаправляем на начальную страницу панели
+                dataManager.InfoFields.SaveInfoField(model);
+                return RedirectToAction(nameof(HomeController.Index),
+                    nameof(HomeController).Replace("Controller", ""));
+            }
+
+            return View(model);
+        }
+    }
+}
